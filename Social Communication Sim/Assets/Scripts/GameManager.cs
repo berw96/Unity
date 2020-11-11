83eeaf0d3db806dd31dbeaefac4b670c927476e1
erw38uob <E.walker5@uni.brighton.ac.uk>
2020-11-11 00:06:02 +0000

    111120-00:07
    Updates .gitignore to exclude SteamVR and other bloated files from commits.
    
    Begun coding ConversationManager, SceneLoader and GameManager.

---
+++ Social Communication Sim/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+    private void Awake()
+    {
+        gm = this;
+    }
+    public void f()
+    {
+        Debug.Log("Game manager here!");
+    }
+}
---
+++ Social Communication Sim/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : GameManager
+{
+    private static Scene[] scenes;
+
+    private void Start()
+    {
+        GameManager.gm.f();
+        registerAllScenes();
+    }
+    private void registerAllScenes()
+    {
+        scenes = new Scene[SceneManager.sceneCountInBuildSettings];
+        Exception e = new Exception("Index off by one error detected.");
+        Debug.Log("Recognized scenes in the build: " + scenes.Length);
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            if (i >= scenes.Length)
+            {
+                throw e;
+            }
+            scenes[i] = SceneManager.GetSceneByBuildIndex(i);
+            Debug.Log("Got a scene: " + scenes[i].name);
+            //Be sure that the scenes you want registered are opened in the hierarchy.
+        }
+    }
+    public void loadScene(int index)
+    {
+        try
+        {
+            SceneManager.LoadScene(scenes[index].name);
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Debug.LogException(e);
+        }
+    }
+    public void exitApplication()
+    {
+        Application.Quit();
+    }
+}
